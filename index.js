85ed7f8edd740418f38c5da39fdb74f37f28708a
Dhanush Patel <dhanush.patel@ymail.com>
2017-08-01 22:33:36 -0700

    added 1 story type
    fable add, will test now

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 process.env.DEBUG = "actions-on-google:*";
 const App = require("actions-on-google").ApiAiApp;
 const admin = require("firebase-admin");
-var serviceAccount = require("../firebasekey.json");
+const jsonfile = require("jsonfile");
+var serviceAccount = require("./firebasekey.json");
 
 // [START YourAction]
 exports.kidsStories = (request, response) => {
@@ -11,19 +12,56 @@ exports.kidsStories = (request, response) => {
   console.log("Request headers: " + JSON.stringify(request.headers));
   console.log("Request body: " + JSON.stringify(request.body));
   // Fulfill action business logic
-  function responseHandler (app) {
+  function inputWelcome (app) {
     // Complete your fulfillment logic and send a response
-    var initMsg = "Hi, welcome to Kids Stories! What would you like to do? You can specify by story type or length."
-    var backups = [
-      "p1",
-      "p2",
-      "p3"
+    var initMsg = "Hi, welcome to Kids Stories! Would you like to hear a fable or a fairy tale?"
+    var backups = ["Would you like to hear a fable or a fairy tale? If you don't care, just say \"random\"! You can also specify the length, by saying, for example: \"long fairy tale\".",
+      "I'm ready when you are! Just say \"fable\", \"fairy tale\", or \"random\" to start listening to a kids bedtime story!",
+      "I can tell you a great kid's bedtime story some other time. Thank you for using Kids Stories!"
     ]; //todo
     app.tell("Hello, World!");
   }
 
+  function giveStory (app) {
+    const storyType = request.body.result.storyType == null ? "fable" : request.body.result.storyType;
+    const storyLength = request.body.result.storyType == null ? "short" : request.body.result.storyType;
+    var file;
+    switch(storyType) {
+      case "fable":
+        file = "./fable.json";
+        break;
+      case "fairytale":
+        file = "./fairytale.json";
+        break;
+      default:
+        //impossible to come here
+    }
+    jsonfile.readFile(file, function(err, stories) {
+      switch(storyType) {
+        case "fable":
+          int storyNum = getRandInt(stories.fable.short.length);
+          app.tell("Here's a great fable:\n\n"+stories.fable.short[storyNum]);
+          break;
+        case "fairytale":
+          int storyNum = (storyLength == "short") ? getRandInt(stories.fairytales.short.length) : getRandInt(stories.fairytales.long.length);
+          app.tell("Here's a great fairy tale:\n\n"+stories.fairy.short[storyNum]);
+          break;
+        default:
+          //impossible to come here
+      }
+    })
+  }
+
+  function getRandInt(max) {
+    var min = 0;
+    var max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+  }
+
+
   const actionMap = new Map();
-  actionMap.set("input.welcome", responseHandler);
+  actionMap.set("input.welcome", inputWelcome);
+  actionMap.set("giveStory", giveStory);
 
   app.handleRequest(actionMap);
 }
